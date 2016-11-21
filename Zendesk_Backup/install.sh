0e079cf78f44d62a2120199654df29c569b8a5de
Sean Cardwell <sean.m.cardwell@gmail.com>
2016-11-21 10:25:48 -0500

    Updated installer script & backup script

--- Zendesk_Backup/install.sh
+++ Zendesk_Backup/install.sh
@@ -14,7 +14,7 @@ sudo pip3 install bs4;
 echo “Everything Installed…”
 echo “Now running backup script…”
 sleep 4
-curl -O https://raw.githubusercontent.com/pyrosousa/DPL-GithubScripts/master/Zendesk_Backup/Zendesk_Backup.py;
+curl -O https://raw.githubusercontent.com/pyrosousa/DPL-GithubScripts/master/py/Zendesk_Backup.py;
 python3 Zendesk_Backup.py;
 rm python-3.5.2-macosx10.6.pkg;
 rm get-pip.py;
